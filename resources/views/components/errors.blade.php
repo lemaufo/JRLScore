4147b67e872326983f5c3b85e99121cc47f095de
leiberjrl <leiberjrl@gmail.com>
2025-02-20 10:10:59 -0600

    integración del componente para manejo de errores y vista de error 404

---
+++ resources/views/components/errors.blade.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="es">
+    <head>
+        <title>Title</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+        <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    </head>
+
+    <body>
+        <header>
+            <!-- place navbar here -->
+        </header>
+        <main>
+            <div id="fondo">
+                <div class="fondo-bg"></div>
+               
+                <div class="error">
+                    {{-- <div>
+                        <i class="{{ $iconError }}"></i>
+                    </div> --}}
+                    <div class="error-404">
+                        <h1>{{ $numError }}</h1>
+                    </div>
+                    <h2>{{ $messageError }}</h2>
+                    <a href="#" class="btn-inicio">Inicio</a>
+                    {{-- <a href="#" class="btn-contacto">Contactos</a> --}}
+                    
+                </div>
+                
+            </div>
+           
+        </main>
+        <footer>
+            <!-- place footer here -->
+        </footer>
+        <!-- Bootstrap JavaScript Libraries -->
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
+</html>
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,3 @@
+<x-errors numError="404" messageError="Lo sentimos!!, la pagina solicitada no fue encontrada">
+
+</x-errors>
